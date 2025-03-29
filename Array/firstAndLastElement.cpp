0a99f0174981c98f521c3b978de11d89452170aa
Arpit Tripathi <tarpit230@gmail.com>
2025-03-29 11:36:25 +0530

    03/29/2025

--- Array/firstAndLastElement.cpp
+++ Array/firstAndLastElement.cpp
@@ -24,13 +24,58 @@ class Solution {
             cout<< num << " ";
         }
     }
+
+    void BetterApproach(vector<int>& nums, int target) {
+        vector<int> result;
+        int low = 0, high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) {
+                result.push_back(mid);
+                int left = mid - 1;
+                while(left >= 0 && nums[left] == target){
+                    result.push_back(left);
+                    left--;
+                }
+                int right = mid+1;
+                while(right < nums.size() && nums[right] == target){
+                    result.push_back(right);
+                    right++;
+                }
+                break;
+            } 
+            else if(nums[mid] > target) {
+                 high = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }  
+        }
+        sort(result.begin(), result.end());
+        if(result.size() == 0) {
+            result.push_back(-1);
+            result.push_back(-1);
+        } else if(result.size() == 1){
+            result.push_back(result[0]);
+        } else {
+            int temp1 = result[0];
+            int temp2 = result[result.size() - 1];
+            result.clear();
+            result.push_back(temp1);
+            result.push_back(temp2);
+        }
+        
+        for(int num: result){
+            cout<< num << " ";
+        }
+    }
 };
 
 int main(){
     vector<int> nums = {5,7,7,8,8,10};
     int target = 8;
     Solution obj;
-    obj.searchRange(nums, target);
+    obj.BetterApproach(nums, target);
 
     return 0;
 }
