f7fa8e14cf8cd5366e31de36a70335d97f00b950
Arpit Tripathi <tarpit230@gmail.com>
2025-08-09 22:35:09 +0530

    09/08/2025

--- LinkedList/LengthofLinkedList.cpp
+++ LinkedList/LengthofLinkedList.cpp
@@ -22,12 +22,81 @@ class Node {
     }
 };
 
+void insertAttail(Node* &tail, int val) {
+    Node* temp = new Node(val);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void insertAtHead(Node* &head, int val){
+    Node* temp = new Node(val);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int lengthofLinkedList(Node* head) {
+    Node* temp = head;
+    int cnt = 0;
+    while(temp != NULL) {
+        cnt++;
+        temp = temp -> next;
+    }
+    return cnt;
+}
+
+bool searchElement(Node* head, int val) {
+    Node* temp = head;
+    while(temp != NULL) {
+        if(temp -> data == val) return true;
+        temp = temp -> next;
+    }
+    return false;
+}
+
+void insertAtPosition(Node* &head, Node* &tail, int position, int val) {
+    if(position == 1) {
+        insertAtHead(head, val);
+        return;
+    }
+    Node* temp = head;
+    int cnt = 1;
+    while (cnt < position - 1 && temp != NULL) {
+        temp = temp -> next;
+        cnt++;
+    }
+    if(temp -> next == NULL) {
+        insertAttail(tail, val);
+        return;
+    }
+    
+    Node* nodeToInsert = new Node(val);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+    
+}
+
 int main() {
     int arr[] = {2, 3, 4, 8};
-    for(int i=0; i<sizeof arr/sizeof arr[0]; i++) {
-        if(i == 0) Node* head = new Node(arr[i]);
-        else {
-            Node* temp = head;
-        }
+    Node* head = new Node(arr[0]);
+    Node* tail = head;
+    int size = sizeof arr/sizeof arr[0];
+    int i = 1;
+    while(i<4) {
+        insertAttail(tail, arr[i]);
+        i++;
     }
+    insertAtPosition(head, tail, 5, 5);
+    print(head);
+    // cout << lengthofLinkedList(head) << endl;
+    // cout << searchElement(head, 6);
+    return 0;
 }
