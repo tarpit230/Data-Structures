0b08e3b177b188ed96865d7bed738279a9d464f4
Arpit Tripathi <tarpit230@gmail.com>
2025-08-15 22:03:49 +0530

    15/08/2025

--- LinkedList/DLL-practice.cpp
+++ LinkedList/DLL-practice.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<stack>
 using namespace std;
 
 class Node
@@ -36,6 +37,7 @@ void print(Node *head)
         cout << temp->data << " ";
         temp = temp->next;
     }
+    cout<< endl;
 }
 
 void insertAtHead(Node *&head, Node *&tail, int val)
@@ -119,14 +121,67 @@ void deleteNode(Node *&head, int position)
     }
 }
 
+Node* reverseDLL(Node* &head) {
+    // Brute force Approach - 
+    // 1. Iterate the linkedlist and push node->data into the stack.
+    // 2. Iterate second time and push stack data from top to linked list.
+    // 3. this will reverse the data in nodes of DLL.
+
+    // Node* temp = head;
+    // stack<int> st;
+    // while(temp != NULL) {
+    //     st.push(temp -> data);
+    //     temp = temp -> next;
+    // }
+    // temp = head;
+    // while(temp != NULL) {
+    //     temp -> data = st.top();
+    //     st.pop();
+    //     temp = temp -> next;
+    // }
+    // return head;
+
+    // Optimal Approach
+    // 1. traverse through linked list and swap the pointers
+    // of each node.
+    // 2. Doing so will reverse linked list in single iteration.
+    // 3. Use curr and last pointer.
+
+    Node* last = NULL;
+    Node* curr = head;
+    while(curr != NULL) {
+        last = curr -> prev;
+        curr -> prev = curr -> next;
+        curr -> next = last;
+        curr = curr -> prev;
+    }
+    if (last != NULL) {
+        head = last->prev;
+    }
+    return head;
+}
+
+int getLength(Node* head) {
+    Node* temp = head;
+    int cnt = 0;
+    while(temp != NULL) {
+        temp = temp -> next;
+        cnt++;
+    }
+    return cnt;
+}
+
 int main()
 {
     Node *node1 = new Node(10);
     Node *head = node1;
     Node *tail = node1;
+    
     insertAtHead(head, tail, 5);
     insertAtTail(head, tail, 7);
     insertAtPosition(head, tail, 2, 9);
-    deleteNode(head, 1);
+    // deleteNode(head, 1);
+    reverseDLL(head);
     print(head);
+    // cout << getLength(head) << endl;
 }
