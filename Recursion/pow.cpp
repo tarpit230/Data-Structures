569c944e0528c1696755dc48b7a37f2c589f420a
Arpit Tripathi <tarpit230@gmail.com>
2025-12-16 00:02:22 +0530

    pow better approach

--- Recursion/pow.cpp
+++ Recursion/pow.cpp
@@ -1,24 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-double myPow(double x, int n) {
+double myPow(double x, int n)
+{
     // Iterative Approach
-    double result = 1.0;
-    if(n<0) {
-        int modifiedN = -n;
-        result = 1 / myPow(x, modifiedN);
+    // double result = 1.0;
+    // if (n < 0)
+    // {
+    //     int modifiedN = -n;
+    //     result = 1 / myPow(x, modifiedN);
+    // }
+
+    // while (n > 0)
+    // {
+    //     result *= x;
+    //     n--;
+    // }
+    // return result;
+
+    // Better Approach
+    if (n == 0)
+        return 1.0;
+
+    long power = n; // to handle Integer.MIN_VALUE
+    if (power < 0)
+    {
+        x = 1 / x;
+        power = -power;
     }
-    
-        while(n > 0) {
+
+    double result = 1.0;
+
+    while (power > 0)
+    {
+        if (power % 2 == 1)
+        {
             result *= x;
-            n--;
         }
-        return result;
+        x *= x;
+        power /= 2;
+    }
+
+    return result;
 }
 
-int main() {
-    double param1 = 1.00000;
-    int param2 = 2147483647;
+int main()
+{
+    double param1 = 2.00000;
+    // int param2 = 2147483647;
+    int param2 = 10;
     cout << myPow(param1, param2);
 
     return 0;
